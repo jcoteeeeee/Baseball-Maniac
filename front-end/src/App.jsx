49a92fb55b5a0a72a6821ed1bb13cc2d4e94bbff
John Cote <johncote61598@gmail.com>
2023-07-24 15:05:37 -0400

    set up userprofilepage and created routing that goes there on both signuppage and loginpage

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes, useNavigate} from 'react-router-dom'
 import Homepage from './components/Homepage.jsx'     
 import SignupPage from './components/SignupPage'   
 import LoginPage from './components/LoginPage' 
+import UserProfilePage from './components/UserProfilePage'    
 
 function App() { 
   const navigate = useNavigate() 
@@ -15,14 +16,22 @@ function App() {
   const goToLogin = () => {
     console.log('click') 
     navigate('/login')
-  }
+  }  
+
+  const goToProfilePage = () => {
+    console.log('click')
+    navigate('/userprofilepage')
+  }  
+
+  console.log(typeof(goToProfilePage)) 
 
   return (
     <>  
       <Routes>
         <Route exact path='/' element={<Homepage goToSignup={goToSignup} goToLogin={goToLogin} />} /> 
-        <Route exact path='/signup' element={<SignupPage/>} />   
-        <Route exact path='login' element={<LoginPage/>} /> 
+        <Route exact path='/signup' element={<SignupPage goToProfilePage={goToProfilePage} />} />   
+        <Route exact path='/login' element={<LoginPage goToProfilePage={goToProfilePage} />} />   
+        <Route exact path='/userprofilepage' element={<UserProfilePage/>} />   
       </Routes>
     </>
   )
---
+++ front-end/src/components/UserProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'  
+
+const UserProfilePage = () => {
+    return(
+        <> 
+            <section>
+
+            </section>
+        </> 
+    )
+}  
+
+export default UserProfilePage 
