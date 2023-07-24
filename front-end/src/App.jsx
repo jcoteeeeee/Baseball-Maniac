1882616ced85b9fe0eb0210dcfb3b7fee5e9bf02
John Cote <johncote61598@gmail.com>
2023-07-24 14:37:01 -0400

    created loginpage with form and then set up routing. Created button on homepage that takes you to loginpage

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes, useNavigate} from 'react-router-dom' 
 import Homepage from './components/Homepage.jsx'     
 import SignupPage from './components/SignupPage'   
+import LoginPage from './components/LoginPage' 
 
 function App() { 
   const navigate = useNavigate() 
@@ -11,11 +12,17 @@ function App() {
     navigate('/signup') 
   }  
 
+  const goToLogin = () => {
+    console.log('click') 
+    navigate('/login')
+  }
+
   return (
     <>  
       <Routes>
-        <Route exact path='/' element={<Homepage goToSignup={goToSignup} />} /> 
+        <Route exact path='/' element={<Homepage goToSignup={goToSignup} goToLogin={goToLogin} />} /> 
         <Route exact path='/signup' element={<SignupPage/>} />   
+        <Route exact path='login' element={<LoginPage/>} /> 
       </Routes>
     </>
   )
